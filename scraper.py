b53c23267a437c5f3becac30474040abf754fc99
shamoil009 <shamoilkhan888@gmail.com>
2023-11-24 15:42:28 +0500

    updtaed

--- scraper.py
+++ scraper.py
@@ -9,26 +9,29 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
-os.environ['PATH']+=r"C:/SeleniumDrivers"
+os.environ["PATH"] += r"C:/SeleniumDrivers"
 # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 
-chromeWebDriverPath="/chromedriver.exe"
+chromeWebDriverPath = "/chromedriver.exe"
 
 driver = webdriver.Chrome()
 # driver.get("https://techwithtim.net")
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+# driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+driver.get("https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html")
+
+
 driver.implicitly_wait(5)
-submit_button=driver.find_element(By.CLASS_NAME,"btn-outline-primary")
+submit_button = driver.find_element(By.ID, "downloadButton")
 submit_button.click()
 
-WebDriverWait(driver,30).until(
+WebDriverWait(driver, 30).until(
     EC.text_to_be_present_in_element(
-        (By.CLASS_NAME,'progress-label'), #element filtration
+        (By.CLASS_NAME, "progress-label"),  # element filtration
         "Complete!"
-        #expected text
+        # expected text
     )
 )
 print(submit_button.text)
 
-sleep(30)
+# sleep(30)
