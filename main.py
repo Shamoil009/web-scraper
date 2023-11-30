a7f91ee57caa4f842b7963790d9aec5b2a34925a
shamoil009 <shamoilkhan888@gmail.com>
2023-11-30 16:13:25 +0500

    updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with Booking() as bot:
     # bot.change_currency(currency="USD")
     bot.select_place_to_go(place_to_go="New York")
     bot.select_dates(check_in_date="2023-12-05", check_out_date="2023-12-24")
-    bot.select_adults()
-    
-    
+    bot.select_adults(count=30)
+
+
 sleep(30)
